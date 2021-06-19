a71f4e444ddcba3c44ce17d91e9315c14e2ec171
Fernando Glingani <fernando.glingani@gmail.com>
2021-06-19 16:09:12 -0300

    extract class

--- config/manifest/extract-test-classes.py
+++ config/manifest/extract-test-classes.py
@@ -10,18 +10,20 @@ path_to_json = "./tests"
 if len(sys.argv) > 1:
     path_to_json = sys.argv[1]
 
-json_files = [pos_json for pos_json in os.listdir(
-    path_to_json) if pos_json.endswith('.json')]
+if os.path.isdir(path_to_json):
 
-for json_file in json_files:
-    json_file = path_to_json + '/' + json_file
-    with open(json_file) as f:
-        data = json.load(f)
-        if data.has_key(key_to_lookup):
-            test_classes += ", ".join(data[key_to_lookup])
-    test_classes += ", "
+    json_files = [pos_json for pos_json in os.listdir(
+        path_to_json) if pos_json.endswith('.json')]
 
-test_classes = test_classes[:-2]
+    for json_file in json_files:
+        json_file = path_to_json + '/' + json_file
+        with open(json_file) as f:
+            data = json.load(f)
+            if data.has_key(key_to_lookup):
+                test_classes += ", ".join(data[key_to_lookup])
+        test_classes += ", "
 
-if len(test_classes) > 0:
-    print(test_classes)
+    test_classes = test_classes[:-2]
+
+    if len(test_classes) > 0:
+        print(test_classes)
